9e031e758aa7478f66b4a810722e0ae7286fcff7
JoaoMarcelino <j.13.marcelino@gmail.com>
2019-11-08 15:26:49 +0000

    Fiz algumas mudanças, mas tem alguns erros

--- Simulador.c
+++ Simulador.c
@@ -8,7 +8,15 @@
 #include <sys/fcntl.h>
 #include <semaphore.h> 
 #include <pthread.h>
+
+#include <errno.h>
+#include <signal.h>
+#include <fcntl.h>
+#include <string.h>
+#include <sys/stat.h>
+
 #define CEM 100
+#define PIPE_NAME   "input_pipe"
 
 typedef struct sharedMemStruct{
 
@@ -21,10 +29,12 @@ void controlTower();
 void flightManager();
 void readConfig();
 
+//SHARED MEMORY
 int shmid;
 memStruct* sharedMemPtr;
 int inputPipe[2];
 
+//CONFIG.TXT
 int unidadeTempo;
 int duracaoDescolagem;
 int intervaloDescolagens;
@@ -35,10 +45,13 @@ int maxHolding;
 int maxPartidas;
 int maxChegadas;
 
+//NAMED PIPE
+int fd_named_pipe;
+
 
 int main() {
-	
-	pipe(inputPipe);
+	int id;
+	pipe(inputPipe); /* unnamed pipe tho */
 
 	
 	if ((shmid = shmget(IPC_PRIVATE, sizeof(memStruct), IPC_CREAT | 0700)) < 1) {
@@ -51,15 +64,21 @@ int main() {
 		exit(1);
 	}
 
-	readConfig();
 
-	if (fork() == 0){
+	printf("check");
+	
+	// wtf is wrong with this?
+	readConfig();
+	if ((id = fork()) == 0){
+		printf("is this right?");
 		controlTower();
 		exit(0);
-	}	
-	
-	flightManager();
-		wait(NULL);
+	}
+	else{	
+		printf("Entering flightManager %d",id);
+		flightManager();
+	}
+	wait(NULL);
 
 	//closing
 	shmctl(shmid,IPC_RMID,NULL);
@@ -89,10 +108,44 @@ void* voos(void* agr){
 }
 
 void controlTower() {
-	
+	printf("Doing work");
+	sleep(2);
+	exit(0);
 }
 
 
 void flightManager() {
+	int Condition=1;
+	char texto[10];
+
+	// PIPE CREATION 
+	unlink(PIPE_NAME);
+	/*Not sure sobre isto*/
+	remove(PIPE_NAME);
+	if((mkfifo(PIPE_NAME,O_CREAT|O_EXCL|0600)<0) && (errno!=EEXIST)){
+		perror("Cannot open named pipe");
+		exit(0);
+	}
+
+	//PIPE OPENING
+	if ((fd_named_pipe = open(PIPE_NAME, O_RDWR)) < 0) {
+		perror("Cannot open pipe for reading: ");
+		exit(0);
+	}
+
+
+	printf("CHeck");
+	while(Condition){
+		read(fd_named_pipe,&texto,10);
+		if(strcmp(texto,"Arrival")==0){
+			printf("FLight arriving this Airport!");
+			Condition=0;
+		}
+
+	}
+	
+	unlink(PIPE_NAME);
+	remove(PIPE_NAME);
+
 
 }
